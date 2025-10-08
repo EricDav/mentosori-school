1370c5ff402ba5f037c4f817b08821505ef1cec2
Folixx Ai <folixxai@gmail.com>
2025-10-08 10:47:06 +0000

    reomve this section "Moments from Our Campus" and make the gallery slide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import HeroSection from '@/components/home/hero-section';
 import AboutSection from '@/components/home/about-section';
-import ImageGallery from '@/components/home/image-gallery';
 import NewsSection from '@/components/home/news-section';
 import ContactSection from '@/components/home/contact-section';
 import GallerySection from '@/components/home/gallery-section';
@@ -10,7 +9,6 @@ export default function Home() {
     <div className="flex flex-col">
       <HeroSection />
       <AboutSection />
-      <ImageGallery />
       <GallerySection />
       <NewsSection />
       <ContactSection />
