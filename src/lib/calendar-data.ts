28ade50863c6855979c8ff3082c59cffc6f00e41
alienyidavid4christ <alienyidavid4christ@gmail.com>
2026-02-09 21:08:40 +0000

    fix school calnder

--- src/lib/calendar-data.ts
+++ src/lib/calendar-data.ts
@@ -1,14 +1,13 @@
 export const schoolCalendar = [
-  { event: "Resumption", date: "Monday 15th September 2025" },
-  { event: "Independence Day", date: "Wednesday 1st Oct, 2025" },
-  { event: "Career Day", date: "Thursday 16th October, 2025" },
-  { event: "Mid-term test", date: "Monday 27th - Friday 31st Oct, 2025" },
-  { event: "Open Day", date: "Thursday 30th Oct, 2025" },
-  { event: "Mid term break", date: "Monday 3rd - Friday 7th Nov, 2025" },
-  { event: "Excursion", date: "Thursday 27th November 2025" },
-  { event: "Examination", date: "Monday 8th - Friday 12th Dec 2025" },
-  { event: "Vacation", date: "Thursday 18th December, 2025" },
   { event: "Resumption", date: "Monday 12th January, 2026" },
+  { event: "Excursion", date: "Thursday 15th January, 2026" },
+  { event: "Haflah", date: "Saturday 7th February, 2026" },
+  { event: "Mid-Term Test", date: "Friday 20th - Tuesday 24th February, 2026" },
+  { event: "Mid-Term Break", date: "Wednesday 25th - Friday 27th February, 2026" },
+  { event: "Ramadan/ Eid Break", date: "Thursday 12th - Monday 24th, 2026" },
+  { event: "Examination", date: "Monday 30th march- Wednesday 8th April, 2026" },
+  { event: "Vacation", date: "Thursday 16th April, 2026" },
+  { event: "Resumption", date: "Monday 4th May, 2026" },
 ];
 
 export const weeklyActivities = [
